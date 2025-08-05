e72a7dd53195544ebd412fb7e1f88b4c45a04a6b
BalaChandra21 <vemulapallibalachandra@gmail.com>
2025-08-05 10:03:14 +0530

    Create CountNameWithoutSpaces.java

---
+++ CountNameWithoutSpaces.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class CountNameWithoutSpaces 
+{ 
+    public static void main(String args[]) 
+	{
+        	Scanner sc = new Scanner(System.in);
+        	System.out.print("Enter your name: ");
+        	String name = sc.nextLine();
+
+        	// To replace white spaces with empty space
+        	String without_space = name.replace(" ", "");
+
+        	// count the no. of characters in given string
+        	int count = without_space.length();
+
+        	// Display no. of characters
+        	System.out.printf("Your name: %s & Total length: %d\n", name, count);
+    	}
+}
